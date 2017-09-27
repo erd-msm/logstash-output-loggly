5d6adcc286f3bbca1e0d31be97e6a8d41a1b154d
erd-msm <E.Degirmencioglu@moneysupermarket.com>
2017-09-27 14:02:52 +0100

    MSM specific changes

--- lib/logstash/outputs/loggly.rb
+++ lib/logstash/outputs/loggly.rb
@@ -2,12 +2,13 @@
 require "logstash/outputs/base"
 require "logstash/namespace"
 require "uri"
+require "stud/buffer"
 # TODO(sissel): Move to something that performs better than net/http
 require "net/http"
 require "net/https"
 
 
-# Ugly monkey patch to get around http://jira.codehaus.org/browse/JRUBY-5529
+# Ugly monkey patch to get around <http://jira.codehaus.org/browse/JRUBY-5529>
 Net::BufferedIO.class_eval do
     BUFSIZE = 1024 * 16
 
@@ -26,48 +27,27 @@ end
 # To use this, you'll need to use a Loggly input with type 'http'
 # and 'json logging' enabled.
 class LogStash::Outputs::Loggly < LogStash::Outputs::Base
+  include Stud::Buffer
   config_name "loggly"
+  milestone 2
 
   # The hostname to send logs to. This should target the loggly http input
-  # server which is usually "logs-01.loggly.com" (Gen2 account).
-  # See Loggly HTTP endpoint documentation at
-  # https://www.loggly.com/docs/http-endpoint/
+  # server which is usually "logs.loggly.com"
   config :host, :validate => :string, :default => "logs-01.loggly.com"
 
   # The loggly http input key to send to.
-  # This is usually visible in the Loggly 'Inputs' page as something like this:
-  # ....
-  #     https://logs-01.loggly.net/inputs/abcdef12-3456-7890-abcd-ef0123456789
+  # This is usually visible in the Loggly 'Inputs' page as something like this
+  #     https://logs.hoover.loggly.net/inputs/abcdef12-3456-7890-abcd-ef0123456789
   #                                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   #                                           \---------->   key   <-------------/
-  # ....
-  # You can use `%{foo}` field lookups here if you need to pull the api key from
+  #
+  # You can use %{foo} field lookups here if you need to pull the api key from
   # the event. This is mainly aimed at multitenant hosting providers who want
   # to offer shipping a customer's logs to that customer's loggly account.
   config :key, :validate => :string, :required => true
 
   # Should the log action be sent over https instead of plain http
   config :proto, :validate => :string, :default => "http"
-
-  # Loggly Tag
-  # Tag helps you to find your logs in the Loggly dashboard easily
-  # You can make a search in Loggly using tag as "tag:logstash-contrib"
-  # or the tag set by you in the config file.
-  #
-  # You can use %{somefield} to allow for custom tag values.
-  # Helpful for leveraging Loggly source groups.
-  # https://www.loggly.com/docs/source-groups/
-  config :tag, :validate => :string, :default => "logstash"
-
-  # Retry count. 
-  # It may be possible that the request may timeout due to slow Internet connection
-  # if such condition appears, retry_count helps in retrying request for multiple times
-  # It will try to submit request until retry_count and then halt
-  config :retry_count, :validate => :number, :default => 5
-
-  # Can Retry.
-  # Setting this value true helps user to send multiple retry attempts if the first request fails
-  config :can_retry, :validate => :boolean, :default => true
 
   # Proxy Host
   config :proxy_host, :validate => :string
@@ -81,105 +61,126 @@ class LogStash::Outputs::Loggly < LogStash::Outputs::Base
   # Proxy Password
   config :proxy_password, :validate => :password, :default => ""
 
+  # This setting controls how many events will be buffered before sending a batch
+  # of events. Note that these are only batched for the same series
+  config :flush_size, :validate => :number, :default => 100
 
-  # HTTP constants
-  HTTP_SUCCESS = "200"
-  HTTP_FORBIDDEN = "403"
-  HTTP_NOT_FOUND = "404"
-  HTTP_INTERNAL_SERVER_ERROR = "500"
-  HTTP_GATEWAY_TIMEOUT = "504"
+  # The amount of time since last flush before a flush is forced.
+  #
+  # This setting helps ensure slow event rates don't get stuck in Logstash.
+  # For example, if your `flush_size` is 100, and you have received 10 events,
+  # and it has been more than `idle_flush_time` seconds since the last flush,
+  # logstash will flush those 10 events automatically.
+  #
+  # This helps keep both fast and slow log streams moving along in
+  # near-real-time.
+  config :idle_flush_time, :validate => :number, :default => 10
+
+  LOGGLY_EVENT_MAX_SIZE = 1024 * 1024
+  LOGGLY_CHUNK_MAX_SIZE = 5 * 1024 * 1024
+
 
   public
   def register
-    # nothing to do
+    buffer_initialize(
+      :max_items => @flush_size,
+      :max_interval => @idle_flush_time,
+      :logger => @logger
+    )
+    @last_count_report = Time.now
+    @events_count = 0
   end
 
   public
   def receive(event)
-    key = event.sprintf(@key)
-    tag = event.sprintf(@tag)
+    return unless output?(event)
+    buffer_receive(event)
 
-    # For those cases where %{somefield} doesn't exist
-    # we should ship logs with the default tag value.
-    tag = 'logstash' if /^%{\w+}/.match(tag)
-
-    # Send event
-    send_event("#{@proto}://#{@host}/inputs/#{key}/tag/#{tag}", format_message(event))
-  end # def receive
-
-  public
-  def format_message(event)
-    event.to_json
+    handle_counts_reporting
   end
 
   private
-  def send_event(url, message)
-    url = URI.parse(url)
+  def handle_counts_reporting
+    @events_count+=1
+
+    secs_since_last_report = Time.now - @last_count_report
+
+    if secs_since_last_report >= 60
+      @last_count_report = Time.now
+      puts "#{Time.now} in last #{secs_since_last_report} #{@events_count} events were retrieved"
+      @events_count = 0
+    end
+  end
+
+  public
+  def flush(events, teardown=false)
+    current_chunk = ""
+    chunks = []
+
+    events.each do |event|
+      if event == LogStash::SHUTDOWN
+        finished
+        return
+      end
+
+      event = event.to_json
+
+      if event.length > LOGGLY_EVENT_MAX_SIZE
+        @logger.warn("Event is too large to send", :event => event)
+        puts "#{Time.now} Event is too large to send, #{event.length}"
+      elsif (current_chunk.length + 1 + event.length) > LOGGLY_CHUNK_MAX_SIZE
+        chunks << current_chunk
+        current_chunk = event
+      else
+        current_chunk << "\n" if current_chunk.length > 0
+        current_chunk << event
+      end
+    end
+
+    chunks << current_chunk if current_chunk.length > 0
+
+    # Send the event over http.
+    url = URI.parse("#{@proto}://#{@host}/bulk/#{@key}")
     @logger.info("Loggly URL", :url => url)
 
-    http = Net::HTTP::Proxy(@proxy_host,
-                            @proxy_port,
-                            @proxy_user,
-                            @proxy_password.value).new(url.host, url.port)
 
-    if url.scheme == 'https'
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    chunks.each do |chunk|
+      handle_chunk(chunk, url)
     end
 
-    request = Net::HTTP::Post.new(url.path)
-    request.body = message
+  end
 
-    # Variable for count total retries
-    totalRetries = 0
+  private
+  def handle_chunk(chunk, url)
+    Thread.new(chunk, url) {|_chunk, _url|
+      retries = 5
 
-    #try posting once when can_retry is false
-    if @can_retry == false
-      @retry_count = 1
-    end
-
-    
-    @retry_count.times do
-    begin
-      response = http.request(request)	
-      case response.code
-	  
-	    # HTTP_SUCCESS :Code 2xx
-	    when HTTP_SUCCESS					
-	      puts "Event send to Loggly"
-		  
-		# HTTP_FORBIDDEN :Code 403
-	    when HTTP_FORBIDDEN					
-	      @logger.warn("User does not have privileges to execute the action.")
-		
-		# HTTP_NOT_FOUND :Code 404
-	    when HTTP_NOT_FOUND					
-	      @logger.warn("Invalid URL. Please check URL should be http://logs-01.loggly.com/inputs/CUSTOMER_TOKEN/tag/logstash")
-	    
-		# HTTP_INTERNAL_SERVER_ERROR :Code 500
-		when HTTP_INTERNAL_SERVER_ERROR			
-	      @logger.warn("Internal Server Error")
-		
-		# HTTP_GATEWAY_TIMEOUT :Code 504
-	    when HTTP_GATEWAY_TIMEOUT				
-	      @logger.warn("Gateway Time Out")
-	    else
-	      @logger.error("Unexpected response code", :code => response.code)
-      end # case
-
-      if [HTTP_SUCCESS,HTTP_FORBIDDEN,HTTP_NOT_FOUND].include?(response.code)	# break the retries loop for the specified response code
-        break
+      http = Net::HTTP::Proxy(@proxy_host, @proxy_port, @proxy_user, @proxy_password.value).new(_url.host, _url.port)
+      if _url.scheme == 'https'
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
-	  rescue StandardError => e
-        @logger.error("An unexpected error occurred", :exception => e.class.name, :error => e.to_s, :backtrace => e.backtrace)
-      end # rescue
-     
-      if totalRetries < @retry_count && totalRetries > 0
-        puts "Waiting for five seconds before retry..."
-        sleep(5)
+
+      begin
+        request = Net::HTTP::Post.new(_url.path)
+        request.body = _chunk
+        response = http.request(request)
+        if response.is_a?(Net::HTTPSuccess)
+          @logger.info("Events sent to Loggly OK!")
+        else
+          puts "Response is not HTTPSuccess"
+          @logger.warn("HTTP error", :error => response.error!)
+        end
+      rescue Exception
+        retries -= 1
+        puts "#{Time.now} Failed to post data to #{_url.path}, #{retries} retries left, #{$!}"
+        sleep 5
+        retry if retries > 0
       end
-	  
-      totalRetries = totalRetries + 1
-    end #loop
-  end # def send_event
+    }
+  end
+
+  def teardown
+    buffer_flush(:final => true)
+  end # def teardown
 end # class LogStash::Outputs::Loggly
